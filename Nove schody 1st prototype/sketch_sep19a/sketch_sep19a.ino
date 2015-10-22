da92f99df569644bef90c47f8d3adecd1b99f835
Ondrej Grosko <ogrosko@gmail.com>
2015-10-22 17:53:59 +0200

    actual state

--- Nove schody 1st prototype/sketch_sep19a/sketch_sep19a.ino
+++ Nove schody 1st prototype/sketch_sep19a/sketch_sep19a.ino
@@ -1,27 +1,30 @@
+#include <Button.h>
 #include <Timer.h>
 #include <LEDFader.h>
 
 // constants won't change. They're used here to
 // set pin numbers:
-const int buttonPin1 = 3;
-const int buttonPin2 = 4;
-const int buttonPin3 = 5;
+//const int buttonPin1 = 3;
+//const int buttonPin2 = 4;
+//const int buttonPin3 = 5;
 
 const int LDR1 = 1;
 const int LDR2 = 2;
 
-const int DARKNESS_TRESHOLD = 750; //[0-1024]
+const int DARKNESS_TRESHOLD = 600; //[0-1024]
 
-const int FADE_IN_DURATION = 500;
-const int FADE_OUT_DURATION = 5000;
+const int FADE_IN_DURATION = 850;
+const int FADE_OUT_DURATION = 4000;
 
 Timer timer;
 LEDFader led1 = LEDFader(6);
 LEDFader led2 = LEDFader(9);
+Button pir1 = Button(3, PULLDOWN);
+Button pir2 = Button(4, PULLDOWN);
+Button pir3 = Button(5, PULLDOWN);
 
-
-int buttonState1 = 0;
-int buttonState2 = 0;
+int pirState1 = 0;
+int pirState2 = 0;
 
 int ldrState1 = 0;
 int ldrState2 = 0;
@@ -30,28 +33,24 @@ int led1OffPointer;
 int led2OffPointer;
 
 void setup() {
-  pinMode(buttonPin1, INPUT);
-  pinMode(buttonPin2, INPUT);
-  Serial.begin(9600);
+  //pinMode(buttonPin1, INPUT);
+  //pinMode(buttonPin2, INPUT);
+  //Serial.begin(9600);
 }
 
 void loop() {
   // read the state of the pushbutton value:
-  buttonState1 = digitalRead(buttonPin1) | digitalRead(buttonPin2);
-  buttonState2 = digitalRead(buttonPin3);
+  pirState1 = pir1.uniquePress() | pir2.uniquePress();
+  pirState2 = pir3.uniquePress();
   
   // read the state of the photoresistors
   ldrState1 = analogRead(LDR1);
   ldrState2 = analogRead(LDR2);
-  Serial.print("LDR1: ");
-  Serial.println(ldrState1);
-  Serial.print("LDR2: ");
-  Serial.println(ldrState2);
   
   //if is dark enviroment
   if(ldrState1 < DARKNESS_TRESHOLD) {
     if  (led1.is_fading() == false) {
-      if (buttonState1 == HIGH) {
+      if (pirState1) {
         if (led1OffPointer) timer.stop(led1OffPointer);
         led1.fade(255, FADE_IN_DURATION); 
       }
@@ -61,7 +60,7 @@ void loop() {
       }
     }
     else {
-      if (buttonState1 == HIGH) {
+      if (pirState1) {
         led1.stop_fade();
         led1.fade(255, FADE_IN_DURATION); 
       }
@@ -77,7 +76,7 @@ void loop() {
   //--------------------------------------
   if(ldrState2 < DARKNESS_TRESHOLD) {
     if  (led2.is_fading() == false) {
-      if (buttonState2 == HIGH) {
+      if (pirState2) {
         led2.fade(255, FADE_IN_DURATION); 
       }
       
@@ -86,7 +85,7 @@ void loop() {
       }
     }
     else {
-      if (buttonState2 == HIGH) {
+      if (pirState2) {
         led2.stop_fade();
         led2.fade(255, FADE_IN_DURATION); 
       }
